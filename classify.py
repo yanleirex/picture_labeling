12917b738d73a2697d5a14885967399e922dedd8
yanleirex <yanleirex@163.com>
2016-08-05 16:02:30 +0800

    first commit

---
+++ classify.py
@@ -0,0 +1,83 @@
+# coding:utf-8
+from __future__ import division
+import time
+import logging
+import sys
+caffe_root = '/home/yanlei/caffe/'
+sys.path.insert(0, caffe_root + 'python')
+import caffe
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+
+logging.getLogger('flower classify')
+logging.basicConfig(level=logging.INFO)
+
+labels = ['pink primrose', 'hard-leaved pocket orchid', 'canterbury bells',
+          'sweet pea', 'english marigold', 'tiger lily', 'moon orchid',
+          'bird of paradise', 'monkshood', 'globe thistle', 'snapdragon',
+          "colt's foot", 'king protea', 'spear thistle', 'yellow iris',
+          'globe-flower', 'purple coneflower', 'peruvian lily', 'balloon flower',
+          'giant white arum lily', 'fire lily', 'pincushion flower', 'fritillary',
+          'red ginger', 'grape hyacinth', 'corn poppy', 'prince of wales feathers',
+          'stemless gentian', 'artichoke', 'sweet william', 'carnation', 'garden phlox',
+          'love in the mist', 'mexican aster', 'alpine sea holly', 'ruby-lipped cattleya',
+          'cape flower', 'great masterwort', 'siam tulip', 'lenten rose', 'barbeton daisy',
+          'daffodil', 'sword lily', 'poinsettia', 'bolero deep blue', 'wallflower',
+          'marigold', 'buttercup', 'oxeye daisy', 'common dandelion', 'petunia',
+          'wild pansy', 'primula', 'sunflower', 'pelargonium', 'bishop of llandaff',
+          'gaura', 'geranium', 'orange dahlia', 'pink-yellow dahlia?', 'cautleya spicata',
+          'japanese anemone', 'black-eyed susan', 'silverbush', 'californian poppy',
+          'osteospermum', 'spring crocus', 'bearded iris', 'windflower', 'tree poppy',
+          'gazania', 'azalea', 'water lily', 'rose', 'thorn apple', 'morning glory',
+          'passion flower', 'lotus', 'toad lily', 'anthurium', 'frangipani', 'clematis',
+          'hibiscus', 'columbine', 'desert-rose', 'tree mallow', 'magnolia', 'cyclamen ',
+          'watercress', 'canna lily', 'hippeastrum ', 'bee balm', 'ball moss', 'foxglove',
+          'bougainvillea', 'camellia', 'mallow', 'mexican petunia', 'bromelia', 'blanket flower',
+          'trumpet creeper', 'blackberry lily']
+
+
+class FlowerClassifier(QThread):
+    result_signal = pyqtSignal(dict)
+    image_path_signal = pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        QThread.__init__(self, parent)
+        logging.info("Initializing FlowerClassifier network")
+        model = 'deploy1.prototxt'
+        weights = 'snapshot_iter_30000_finetune.caffemodel'
+        self.net = caffe.Net(model, weights, caffe.TEST)
+        self.transformer = caffe.io.Transformer({'data': self.net.blobs['data'].data.shape})
+        self.transformer.set_transpose('data', (2, 0, 1))
+        self.transformer.set_channel_swap('data', (2, 1, 0))
+        self.transformer.set_raw_scale('data', 255)
+        self.net.blobs['data'].reshape(1, 3, 227, 227)
+        self.image_to_recognition = list()
+
+    def classify(self, image_path):
+        self.image_path_signal.emit(image_path)
+        # logging.info("classify image")
+        start = time.time()
+        image = caffe.io.load_image(image_path)
+        self.net.blobs['data'].data[...] = self.transformer.preprocess('data', image)
+        self.net.forward()
+        prob = self.net.blobs['prob'].data[0]
+        top_k = self.net.blobs['prob'].data[0].flatten().argsort()[-1:-6:-1]
+        end = time.time()
+        top_prob = [prob[index] for index in top_k]
+        # logging.info("Time cost: {0}".format(end - start))
+        result = list()
+        for i in range(len(top_k)):
+            item = {"label": labels[top_k[i]], "prob": top_prob[i]}
+            result.append(item)
+        results = {"result": result, "time": end-start}
+        self.result_signal.emit(results)
+
+    def run(self):
+        if self.image_to_recognition:
+            for image in self.image_to_recognition:
+                self.classify(image)
+
+    def set_image(self, images):
+        self.image_to_recognition = images
---
+++ main.py
@@ -0,0 +1,213 @@
+# -*- coding:utf-8 -*-
+import os
+import sys
+import glob
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+from classify import FlowerClassifier
+
+QTextCodec.setCodecForTr(QTextCodec.codecForName('utf8'))
+
+
+class MainDialog(QDialog):
+    def __init__(self, parent=None):
+        super(MainDialog, self).__init__(parent)
+        self.classifier = FlowerClassifier()
+
+        self.setWindowTitle(self.tr("图像打标工具"))
+        self.setFixedSize(QSize(650, 450))
+
+        self.current_non_label_image_path = ''
+        self.current_reference_image_path = ''
+        self.auto_image_path = ''
+        self.auto_images = []
+        self.all_images = []
+        self.image_index = 0
+        self.num_of_image = 0
+
+        # show image label
+        self.reference_image = QLabel()
+        self.non_label_image = QLabel()
+
+        # open_image_button = QPushButton(self.tr("打开图像"))
+        radio_button_manual = QRadioButton(self.tr("手动"))
+        radio_button_auto = QRadioButton(self.tr("自动"))
+        radio_button_manual.setChecked(True)
+
+        self.imageLabelQLabel = QLineEdit()
+        self.current_path = QLabel()
+        # self.current_path_ = QLabel(self.tr("当前图片："))
+        # self.current_path_.setAlignment(Qt.AlignRight)
+        # current_path_layout = QHBoxLayout()
+        # current_path_layout.addWidget(self.current_path_)
+        # current_path_layout.addWidget(self.current_path)
+        self.info_text = QTextEdit()
+
+        # manual layout
+        manual_layout_h_ = QHBoxLayout()
+        self.manual_layout_reference_image = QPushButton(self.tr("参考图像"))
+        self.manual_layout_non_label_image = QPushButton(self.tr("待识别图像"))
+        manual_layout_h_.addWidget(self.manual_layout_reference_image)
+        manual_layout_h_.addWidget(self.manual_layout_non_label_image)
+
+        manual_layout_h = QHBoxLayout()
+        self.manual_layout_line_edit = QLineEdit()
+        self.manual_layout_line_edit_reference = QLineEdit()
+        self.manual_layout_line_edit_reference.setEnabled(False)
+        manual_layout_h.addWidget(QLabel(self.tr("参考标签")))
+        manual_layout_h.addWidget(self.manual_layout_line_edit_reference)
+        manual_layout_h.addWidget(QLabel(self.tr("标签")))
+        manual_layout_h.addWidget(self.manual_layout_line_edit)
+
+        self.manual_layout_previous_image = QPushButton(self.tr("上一张"))
+        self.manual_layout_next_image = QPushButton(self.tr("下一张"))
+        manual_layout_h_image_button = QHBoxLayout()
+        manual_layout_h_image_button.addWidget(self.manual_layout_previous_image)
+        manual_layout_h_image_button.addWidget(self.manual_layout_next_image)
+        manual_layout = QVBoxLayout()
+        manual_layout.addLayout(manual_layout_h_)
+        manual_layout.addLayout(manual_layout_h)
+        manual_layout.addLayout(manual_layout_h_image_button)
+
+        # auto layout
+        auto_layout = QVBoxLayout()
+        self.auto_info_text = QTextEdit()
+        self.set_path_button = QPushButton(self.tr("选择路径"))
+        self.recognize_button = QPushButton(self.tr("识别"))
+        auto_layout_button_h = QHBoxLayout()
+        auto_layout_button_h.addWidget(self.set_path_button)
+        auto_layout_button_h.addWidget(self.recognize_button)
+        auto_layout.addWidget(self.auto_info_text)
+        auto_layout.addLayout(auto_layout_button_h)
+
+        grid_layout = QGridLayout()
+        grid_layout.addWidget(self.reference_image, 0, 0)
+        grid_layout.addWidget(self.non_label_image, 0, 1)
+        grid_layout.addWidget(radio_button_auto, 1, 0)
+        grid_layout.addWidget(radio_button_manual, 1, 1)
+        grid_layout.addLayout(auto_layout, 2, 0)
+        grid_layout.addLayout(manual_layout, 2, 1)
+
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addLayout(grid_layout)
+        # vertical_layout.addLayout(current_path_layout)
+        vertical_layout.addWidget(self.current_path)
+        vertical_layout.addWidget(self.info_text)
+
+        self.setLayout(vertical_layout)
+        self.set_init_image()
+
+        # connect signal slot
+        # self.connect(open_image_button, SIGNAL("clicked()"), self.open_file)
+        self.connect(self.manual_layout_reference_image, SIGNAL("clicked()"), self.set_reference_image)
+        self.connect(self.manual_layout_non_label_image, SIGNAL("clicked()"), self.set_image)
+        self.connect(self.manual_layout_previous_image, SIGNAL("clicked()"), self.open_previous_image)
+        self.connect(self.manual_layout_next_image, SIGNAL("clicked()"), self.open_next_image)
+        self.connect(self.recognize_button, SIGNAL("clicked()"), self.auto_label)
+        self.connect(self.set_path_button, SIGNAL("clicked()"), self.set_auto_path)
+        self.classifier.result_signal.connect(self.show_result)
+        self.classifier.image_path_signal.connect(self.show_image)
+
+    def set_init_image(self, reference_image_path=None, image_path=None):
+        if reference_image_path is None:
+            reference_image_path = "/home/yanlei/Pictures/yellow-iris-by-wiki-commons.jpg"
+        else:
+            reference_image_path = reference_image_path
+        if image_path is None:
+            image_path = "/home/yanlei/Pictures/3813191262_ecfcdfab8f_b.jpg"
+        else:
+            image_path = image_path
+        self.show_reference_image(reference_image_path)
+        self.show_image(image_path)
+
+    def show_reference_image(self, reference_image_path):
+        # fixed image size
+        if reference_image_path is not None:
+            image_fixed_size = QSize(320, 200)
+            picture = QPixmap()
+            picture.load(reference_image_path)
+            self.reference_image.setPixmap(picture.scaled(image_fixed_size))
+
+    def show_image(self, image_path):
+        if image_path is not None:
+            self.set_current_path_info(image_path)
+            image_fixed_size = QSize(320, 200)
+            picture = QPixmap()
+            picture.load(image_path)
+            self.non_label_image.setPixmap(picture.scaled(image_fixed_size))
+
+    def open_file(self):
+        s = QFileDialog.getOpenFileName(self, self.tr("选择文件"), "/home", "*.*")
+        self.current_path.setText(str(s))
+        self.set_info(str(s))
+
+    def set_reference_image(self):
+        s = QFileDialog.getOpenFileName(self, self.tr("选择图像文件"), "/home", "*.*")
+        if s:
+            self.current_reference_image_path = str(s)
+            self.show_reference_image(reference_image_path=str(s))
+        else:
+            self.set_init_image()
+
+    def set_image(self):
+        s = QFileDialog.getOpenFileName(self, self.tr("选择图像文件"), "/home", "*.*")
+        if s:
+            self.current_non_label_image_path = str(s)
+            path = os.path.split(self.current_non_label_image_path)[0]
+            self.all_images = glob.glob(path + '/*.*')
+            self.num_of_image = len(self.all_images)
+            self.image_index = 0
+            self.show_image(image_path=str(s))
+        else:
+            self.set_init_image()
+
+    def open_previous_image(self):
+        if 0 <= self.image_index <= self.num_of_image:
+            if self.image_index is self.num_of_image:
+                self.image_index -= 1
+            path = self.all_images[self.image_index]
+            self.set_current_path_info(path)
+            if self.image_index is not 0:
+                self.image_index -= 1
+            self.show_image(path)
+
+    def open_next_image(self):
+        if 0 <= self.image_index < self.num_of_image:
+            path = self.all_images[self.image_index]
+            self.set_current_path_info(path)
+            self.image_index += 1
+            self.show_image(path)
+
+    def auto_label(self):
+        self.classifier.set_image(self.auto_images)
+        self.classifier.start()
+
+    def set_auto_path(self):
+        s = QFileDialog.getExistingDirectory(self, self.tr("选择图像文件夹"), "/home")
+        self.auto_image_path = str(s)
+        self.auto_images = glob.glob(self.auto_image_path + '/*.*')
+
+    def set_info(self, info):
+        self.info_text.append(info)
+
+    def set_auto_info(self, info):
+        self.auto_info_text.clear()
+        self.auto_info_text.setText(info)
+
+    def set_current_path_info(self, text):
+        self.current_path.setText(self.tr("当前图片：") + text)
+
+    def show_result(self, results):
+        result = results['result']
+        time = results['time']
+        result_str = "Label:{0}\nProb:{1}\nTime:{2} ms".format(result[0]['label'], result[0]['prob'], str(time))
+        self.set_auto_info(result_str)
+        self.set_info(result_str)
+
+
+app = QApplication(sys.argv)
+dialog = MainDialog()
+dialog.show()
+app.exec_()
